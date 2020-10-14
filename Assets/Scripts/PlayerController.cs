4006fadfc30911b0f3e7e80216279e51d6ded5b5
Uni-Dev-Tech <64417125+Uni-Dev-Tech@users.noreply.github.com>
2020-10-14 13:18:26 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float resX, resZ; // для хранения Input с клавиатуры
+    [Header("Скорость движения ГГ")][SerializeField] private float speed;
+    [Header("Время через которое появится игрок(секунды)")] [SerializeField] private float timeAppear;
+    [SerializeField] private MeshRenderer body, head, cap_1, cap_2;
+    private Rigidbody rb;
+    [SerializeField] private ParticleSystem dust;
+    private bool onGround;// ограничивает воспроизведение партиклов
+    [HideInInspector] public bool catched; // блокирует управление после проигрыша
+    private Animator animator;
+    private void Start()
+    {
+        catched = false;
+        onGround = false;
+        StartCoroutine(PlayerAppear());
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+    void FixedUpdate()
+    {
+        resX = Input.GetAxisRaw("Vertical");
+        resZ = Input.GetAxisRaw("Horizontal");
+        Vector3 move = new Vector3(-resX, 0, resZ);
+        if (!catched)
+        {
+            rb.AddForce(move * speed * Time.fixedDeltaTime, ForceMode.Impulse);
+            transform.LookAt(transform.position + new Vector3(resZ, 0, resX)); // поворачиваем игрока в сторону движения
+        }
+    }
+    /// <summary>
+    /// Активирует игрока через заданное время
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator PlayerAppear()
+    {
+        yield return new WaitForSeconds(timeAppear);
+        body.enabled = true;
+        head.enabled = true;
+        cap_1.enabled = true;
+        cap_2.enabled = true;
+        GetComponent<CapsuleCollider>().enabled = true;
+        rb.useGravity = true;
+        rb.isKinematic = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // единожды воспроизводит сист частиц
+        if(!onGround)
+            if(collision.gameObject.CompareTag("Floor"))
+            {
+                dust.Play();
+                onGround = true;
+            }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //После прохождения игрока к финишу блокирует управление, передвигает игрока в центр
+        //тригера, проигрывает анимацию и запускает панель выигрыша 
+        if (other.CompareTag("Finish"))
+        {
+            catched = true;
+            speed = 0;
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            transform.rotation = Quaternion.identity;
+            do
+            {
+                transform.position = Vector3.MoveTowards(transform.position, 
+                    new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), 1f);
+            } while (transform.position != new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z));
+            animator.SetTrigger("EndLevel");
+            StartCoroutine(UIManager.instance.SelectUI(UIManager.instance.UIPanels[1]));
+        }
+    }
+}
